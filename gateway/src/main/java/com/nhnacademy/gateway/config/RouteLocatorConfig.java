0f996a86072a36bfc668bb2608969163ac3c7070
Splleat <Splleat@gmail.com>
2026-07-10 15:28:19 +0900

    feat: 유레카, 게이트웨이 초기 설정

---
+++ gateway/src/main/java/com/nhnacademy/gateway/config/RouteLocatorConfig.java
@@ -0,0 +1,18 @@
+package com.nhnacademy.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteLocatorConfig {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("team1-front",
+                        p -> p.path("/**").uri("lb://team1-front"))
+                .build();
+    }
+}

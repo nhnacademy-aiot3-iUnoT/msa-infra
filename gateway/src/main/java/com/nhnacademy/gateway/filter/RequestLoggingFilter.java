348002a5fe34d9ba1fb3dbe74a190b427da56701
Splleat <Splleat@gmail.com>
2026-07-22 15:10:03 +0900

    feat: RequestLoggingFilter 추가

---
+++ gateway/src/main/java/com/nhnacademy/gateway/filter/RequestLoggingFilter.java
@@ -0,0 +1,26 @@
+package com.nhnacademy.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class RequestLoggingFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("요청 수신: {} {}", exchange.getRequest().getMethod(), exchange.getRequest().getURI());
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}

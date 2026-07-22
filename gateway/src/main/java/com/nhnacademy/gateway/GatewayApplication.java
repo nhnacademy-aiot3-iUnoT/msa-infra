5a031a74cc0c3ce35dc64072a58ce087486df982
Splleat <splleat@gmail.com>
2026-07-22 23:06:10 +0900

    fix: GatewayApplication Reactor 자동 컨텍스트 전파 설정 추가

--- gateway/src/main/java/com/nhnacademy/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/nhnacademy/gateway/GatewayApplication.java
@@ -3,12 +3,14 @@ package com.nhnacademy.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import reactor.core.publisher.Hooks;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
+        Hooks.enableAutomaticContextPropagation();
         SpringApplication.run(GatewayApplication.class, args);
     }
 

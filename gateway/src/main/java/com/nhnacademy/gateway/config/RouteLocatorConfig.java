6658519c91fbe1b9b2689dbda784eccc15c1fd93
Splleat <splleat@gmail.com>
2026-07-22 22:02:22 +0900

    fix: gateway team1-front 라우트 설정 제거

--- gateway/src/main/java/com/nhnacademy/gateway/config/RouteLocatorConfig.java
+++ gateway/src/main/java/com/nhnacademy/gateway/config/RouteLocatorConfig.java
@@ -13,8 +13,6 @@ public class RouteLocatorConfig {
         return builder.routes()
                 .route("team1-rule-engine",
                         p -> p.path("/api/rule-engine/**").uri("lb://rule-engine"))
-                .route("team1-front",
-                        p -> p.path("/**").uri("lb://team1-front"))
                 .build();
     }
 }
